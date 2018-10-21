e0cdfd6571495fd701c4b0d8703b9b4c611f9981
Islam Bayoumy <islam.bayoumy@gmail.com>
2018-10-21 12:23:33 +0200

    refactoring

--- list_reducer.py
+++ list_reducer.py
@@ -2,12 +2,14 @@ import random
 from datetime import datetime as dt
 import matplotlib.pyplot as plt
 
+
 def avg_dates(date1, date2) -> str:
     d1 = dt.strptime(str(date1), '%Y-%m-%d')
     d2 = dt.strptime(str(date2), '%Y-%m-%d')
     avg_date = (d1.timestamp() + d2.timestamp()) / 2
 
     return dt.fromtimestamp(int(avg_date)).strftime("%Y-%m-%d")
+
 
 def reduce_list(values, dates, length) -> tuple:
     if len(values) <= length:
@@ -21,40 +23,42 @@ def reduce_list(values, dates, length) -> tuple:
             reduced_dates.append(temp_date)
         return reduce_list(reduced_values, reduced_dates, length)
 
+
 def draw_line(values, labels, new_values, new_labels):
     plt.figure(figsize=(10, 8))
 
-    plt.subplot(3,1,1)
+    plt.subplot(3, 1, 1)
     plt.plot(labels, values, 'r')
     plt.title('Old List')
     plt.xticks(rotation=45)
 
-    plt.subplot(3,1,3)    
+    plt.subplot(3, 1, 3)
     plt.plot(new_labels, new_values)
     plt.title('Reduced List')
 
     plt.show()
+
 
 def test_fun():
     values = random.sample(range(1, 100), 20)
     values.sort(reverse=False)
 
     dates = ['2015-01-01', '2015-06-01',
-            '2015-07-01', '2015-10-01',
-            '2015-12-01', '2016-02-01',
-            '2016-04-01', '2016-05-01',
-            '2016-08-01', '2016-10-01',
-            '2016-11-01', '2017-02-01',
-            '2017-03-01', '2017-06-01',
-            '2017-09-01', '2017-11-01',
-            '2018-01-01', '2018-02-01',
-            '2018-04-01', '2018-06-01']
+             '2015-07-01', '2015-10-01',
+             '2015-12-01', '2016-02-01',
+             '2016-04-01', '2016-05-01',
+             '2016-08-01', '2016-10-01',
+             '2016-11-01', '2017-02-01',
+             '2017-03-01', '2017-06-01',
+             '2017-09-01', '2017-11-01',
+             '2018-01-01', '2018-02-01',
+             '2018-04-01', '2018-06-01']
 
     length = 5
 
     new_values, new_dates = reduce_list(values, dates, length)
 
-    assert(len(new_values)<=length)
+    assert(len(new_values) <= length)
 
     print("Original list: {} \n {}".format(values, dates))
     print("Original list length: {}".format(len(values)))
@@ -63,5 +67,6 @@ def test_fun():
     print("Reduced list length: {}".format(len(new_values)))
 
     draw_line(values, dates, new_values, new_dates)
+
 
 test_fun()
